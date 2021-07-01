0ab1281b980db4cebbba75d141c4bfd37a3999da
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-07-01 09:49:55 +0200

    heure

--- sameo-shop/client/src/Components/Header.js
+++ sameo-shop/client/src/Components/Header.js
@@ -1,21 +1,34 @@
 import '../CSS/Header.scss';
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import homeLogo from '../Images/logo-maison.png';
 import logotxt from '../Images/Logo-txt.png';
+import {useState, useEffect} from 'react';
 
 function Header(){
-    // const history = useHistory();
-    // const handleAdmin=()=>{
-    //     history.push("/admin");
-    // };
+
+    const [time, setTime] = useState('');
+
+
+    useEffect(() => {
+        var time = new Date().toLocaleTimeString();
+        setTime(time);
+
+        const interval = setInterval(() => {
+            time = new Date().toLocaleTimeString();
+            setTime(time);
+        }, 1000);
+
+        return () => clearInterval(interval);
+
+    }, [])
+
 
     return (
         <header>
             <div className="Header">
+                <p className="header-time">{time}</p>
                 <img src={logotxt} alt="La Couture du Charme" className="header_logo"/>
-                {/* <h1>La Couture du Charme</h1> */}
                 <div className="header-right">
                     <h2>Saméo-Shop</h2>
                     <Link to={"/"}><img src={homeLogo} alt="Home" className="header_img"/></Link>
