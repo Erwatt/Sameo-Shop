7e873b7befc8fa7c2f28a4eba7afd1bec9c54ec6
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2021-09-23 18:11:15 +0200

    css admin message

--- sameo-shop/client/src/Components/AdminMessage.js
+++ sameo-shop/client/src/Components/AdminMessage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import Services from '../services';
+import {Link} from 'react-router-dom';
+import arrow from '../Images/arrow.png';
+import '../CSS/AdminMessage.css';
 
 function AdminMessage(){
     const [message, setMessage] = useState('');
@@ -17,16 +20,17 @@ function AdminMessage(){
     };
 
     return(
-    <div>
+    <div className="adminMessage">
+        <Link className="customerMessage_retours" to="/Admin"><img src={arrow} alt="Retour" className="customerMessage_retour"/><h4 className="customerMessage_retour_txt">Retour admin</h4></Link>
         <form onSubmit={() => handleSend()}>
-            <select name="customersList"  onChange={(e) => setSelectedCustomer(e.target.value)}>
+            <select className="adminMessage_customersList" name="customersList"  onChange={(e) => setSelectedCustomer(e.target.value)}>
                 <option>Choisissez un client</option>
                 {customersList.map(({name, firstname}) => (
                     <option value={name} key={name}>{name} {firstname}</option>
                 ))}
             </select>
-            <textarea placeholder="Message" onChange={(e) => setMessage(e.target.value)}/>
-            <button type="submit">Envoyer</button>
+            <textarea placeholder="Message" className="adminMessage_message" onChange={(e) => setMessage(e.target.value)}/>
+            <button type="submit" className="adminMessage_button">Envoyer</button>
         </form>
     </div>
     );
